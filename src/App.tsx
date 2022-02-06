987c2c379f246f44dd09972c2f78c1c448d71697
talkiedoorkey <89324532+talkiedoorkey@users.noreply.github.com>
2022-02-06 10:26:26 +0900

    Making No56

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
 
-type TodoType = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
+const user: User = {
+  name: "まさき",
+  hobbies: ["映画", "ゲーム"]
 };
 
 export default function App() {
@@ -22,9 +24,12 @@ export default function App() {
   };
   return (
     <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
+          key={todo.id}
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
